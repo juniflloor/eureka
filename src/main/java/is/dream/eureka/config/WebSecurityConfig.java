2a0ca857df753e419f1e3a731d3fd37bfb4417b2
chendongzhao <2457364447@qq.com>
2020-06-08 21:40:28 +0800

    cause eureka context-path trigger eureka client register fail (registiation status 404)

---
+++ src/main/java/is/dream/eureka/config/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package is.dream.eureka.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author chendongzhao
+ * @version 1.0
+ * @date 2020/6/8 20:33
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+    }
+}
